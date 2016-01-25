562350210b3496b2d46f3af09013c3c6ced5c50a
rathero <curs@c6s309pc41.fib.upc.es>
2016-01-25 10:55:37 +0100

    Usando api para obtener ciudades, no acabado

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('CitiesCtrl', function($scope, Cities) {
+.controller('CitiesCtrl', function($scope, $http, Cities) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -14,12 +14,31 @@ angular.module('starter.controllers', [])
   $scope.remove = function(city) {
     Cities.remove(city);
   };
+  $scope.search = function(searchWord, $http){
+    if(searchWord != ""){
+      $scope.cities = new Array();
+      var request = "http://api.openweathermap.org/data/2.5/find?q=" + searchWord + "&type=like&mode=xml&appid=44db6a862fba0b067b1930da0d769e98";
+        $http.get(request).success(function(data, status, headers, config) {
+           data.cities.list.forEach(function(entry) {
+              var cityFound = data.cities.list[i];
+              console.log(cityFound);
+              var city = {};
+              city.id = 0;
+              city.name = searchWord;
+              $scope.cities.push(city);
+           });
+         });
+    }
+    else $scope.cities = Cities.all();
+  };
+  $scope.resetCities = function(){
+    $scope.cities = Cities.all();
+  };
 })
 
 .controller('CityDetailCtrl', function($scope, $stateParams, Cities, $http) {
   $scope.city = Cities.get($stateParams.cityId);
   var request = "http://api.openweathermap.org/data/2.5/weather?q=" + $scope.city.name + ",es&APPID=155de3b370efd8e0f45c05494dfb08e9";
-  console.log(request);
   $http.get(request).success(function(data, status, headers, config) {
     $scope.city.details = data;
   });
